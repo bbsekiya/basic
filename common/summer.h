fea4871dd3ee7414f077ef73dd553b820062470e
bbsekiya <brian210003@gmail.com>
2015-10-09 18:18:19 -0700

    updated status.h, resolved conflict

--- common/summer.h
+++ common/summer.h
@@ -1,10 +1,16 @@
 //hot fix 071715
 // 10-9-15 : int x1, int x2 added in the class
+//           added constructor
 #include <string>
+
 class Summer
 {
 	int x1;
 	int x2;
 
 	std::string mystring;
+
+	Summer(int n1, int n2) : x1(n1), x2(n2) {}
+
 };
+
