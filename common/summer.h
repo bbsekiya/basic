d71104db31e2aeb9af3ad0811935294d875c0998
bbsekiya <brian210003@gmail.com>
2015-11-06 13:21:58 -0800

    added n2.h and updated summer.h

--- common/summer.h
+++ common/summer.h
@@ -1,8 +1,9 @@
 //hot fix 071715
 // 10-9-15 : int x1, int x2 added in the class
 //           added constructor
+// 11-05-15 : added shared_ptr include
 #include <string>
-
+#include <boost/shared_ptr>
 class Summer
 {
 	int x1;
@@ -10,6 +11,7 @@ class Summer
 
 	std::string mystring;
 
+	
 	Summer(int n1, int n2) : x1(n1), x2(n2) {}
 
 };
